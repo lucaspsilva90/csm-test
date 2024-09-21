2e7bf4638df407f17d8c22e5a509c86d60046154
Lucas Silva <lucas.p.silva90@hotmail.com>
2024-09-21 11:51:38 -0300

    chore: usefilter in start-stream controller

--- src/infra/http/csm/start-stream.controller.ts
+++ src/infra/http/csm/start-stream.controller.ts
@@ -1,33 +1,20 @@
-import { Body, ConflictException, Controller, InternalServerErrorException, NotFoundException, Post, ValidationPipe } from '@nestjs/common';
-import { ResourceLocked } from 'src/domain/errors/resource-locked-error';
-import { StreamLimitReachedError } from 'src/domain/errors/stream-limit-reached-error';
-import { UserSessionNotFoundError } from 'src/domain/errors/user-session-not-found-error';
+import { Body, Controller, Post, UseFilters, ValidationPipe } from '@nestjs/common';
+
 import { StartStreamUseCase } from 'src/domain/use-cases/start-stream';
 import { StartStreamDTO, StartStreamResponseDTO } from 'src/infra/dto/start-stream.dto';
+import { DomainExceptionFilter } from '../filters/domain-exception-filter';
 
 @Controller('session')
 export class StartStreamController {
     constructor(private startStreamUseCase: StartStreamUseCase) { }
 
     @Post('/stream/start')
+    @UseFilters(DomainExceptionFilter)
     async handle(@Body(new ValidationPipe()) { userId }: StartStreamDTO): Promise<StartStreamResponseDTO> {
-        try {
-            const { message } = await this.startStreamUseCase.execute({ userId });
+        const { message } = await this.startStreamUseCase.execute({ userId });
 
-            return {
-                message
-            }
-        } catch (error) {
-            if (error instanceof UserSessionNotFoundError) {
-                throw new NotFoundException({ message: error.message });
-            }
-            if (error instanceof StreamLimitReachedError) {
-                throw new ConflictException({ message: error.message });
-            }
-            if(error instanceof ResourceLocked) {
-                throw new ConflictException({ message: error.message, additionalData: error.additionalData });
-            }
-            throw new InternalServerErrorException({ message: error.message, errorStack: error.stack });
+        return {
+            message
         }
     }
 }
